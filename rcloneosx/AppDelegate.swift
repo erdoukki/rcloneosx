33e424469d0320047103c32baaca4c4cfda8fb10
Thomas Evensen <thomeven@gmail.com>
2017-11-05 20:48:26 +0100

    Initial Commit

---
+++ rcloneosx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  rcloneosx
+//
+//  Created by Thomas Evensen on 05.11.2017.
+//  Copyright © 2017 Thomas Evensen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
