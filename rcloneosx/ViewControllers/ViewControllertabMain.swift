285abab34cb35ad17588dc3abcc2c2f5cadc9360
Thomas Evensen <thomeven@gmail.com>
2017-11-12 19:12:31 +0100

    color move red

--- rcloneosx/ViewControllers/ViewControllertabMain.swift
+++ rcloneosx/ViewControllers/ViewControllertabMain.swift
@@ -456,6 +456,12 @@ extension ViewControllertabMain: NSTableViewDelegate {
                     let range = (text! as NSString).range(of: text!)
                     attributedString.addAttribute(NSAttributedStringKey.foregroundColor, value: NSColor.systemBlue, range: range)
                     return attributedString
+                } else if self.configurations!.getConfigurations()[row].task == "move"{
+                    text = object[tableColumn!.identifier] as? String
+                    let attributedString = NSMutableAttributedString(string: (text!))
+                    let range = (text! as NSString).range(of: text!)
+                    attributedString.addAttribute(NSAttributedStringKey.foregroundColor, value: NSColor.red, range: range)
+                    return attributedString
                 } else {
                    return object[tableColumn!.identifier] as? String
                 }
